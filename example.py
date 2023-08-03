c9bca9602b1de1e653975cd628f6caa2785e8cfc
limengxun <limengxun45@outlook.com>
2023-08-03 21:51:41 +0800

    fix: zero division

--- example.py
+++ example.py
@@ -1,5 +1,7 @@
 import random
 from tiny_vectordb import VectorDatabase
+import os
+random.seed(0)
 
 # VectorDatabase.VERBOSE = True
 
@@ -14,19 +16,39 @@ collection_configs = [
     }
 ]
 
+if os.path.exists("test.db"):
+    if input("test.db exists, delete it? (y/n)") == "y":
+        os.remove("test.db")
+    else:
+        exit("This script will raise error if test.db exists, for duplicate item addition")
+
+print("Compiling...")
 database = VectorDatabase("test.db", collection_configs)
 
 collection = database["hello"]
 
-# 50 vectors of 256 dimension
-vectors = [[random.random() for _ in range(256)] for _ in range(50)]
-vector_ids = [f"vector_{i}" for i in range(50)]
+# 100 vectors of 256 dimension
+print("Creating vectors...")
+N_VECTORS = 20
+vectors = [[random.random() for _ in range(256)] for _ in range(N_VECTORS)]
+vector_ids = [f"vector_{i}" for i in range(N_VECTORS)]
 
 # io operations, may encounter duplicate ids if you run this script multiple times
+print("Adding vectors...")
 collection.addBlock(vector_ids, vectors)
 collection.deleteBlock(vector_ids[:10:2])
 
-search_ids, search_scores = collection.search([random.random() for _ in range(256)], k=10)
+# set block, will have no error if id not exists
+collection.setBlock(
+    vector_ids[10:20] + [f"addtional{i}" for i in range(3)], 
+    [[0. for _ in range(256)] for _ in range(13)]
+)
 
+print("Current keys:", collection.keys())
+
+search_ids, search_scores = collection.search([random.random() for _ in range(256)], k=10)
+print("Top 10 search results:", search_ids, search_scores)
+
+print("Flushing and commiting...")
 database.flush()
 database.commit()
--- tiny_vectordb/include/searchAlgorithm.hpp
+++ tiny_vectordb/include/searchAlgorithm.hpp
@@ -8,13 +8,14 @@ std::vector<int> topKIndexes(const Eigen::Vector<float, Eigen::Dynamic> scores, 
 /* target: (N, feat_dim), query: (feat_dim, ) */
 template <typename NumT>
 inline Eigen::Vector<float, Eigen::Dynamic> cosineSimilarity(const MatrixF& target, const Eigen::Vector<NumT, FEAT_DIM>& query_matrix){
+    const fp32 _eps = 1e-8;
     if (target.cols() != query_matrix.size()){
         throw std::runtime_error("query size not match");
     }
     Eigen::Matrix<NumT, Eigen::Dynamic, 1> search_scores = target * query_matrix;
     float norm_query = query_matrix.norm();
     Eigen::Vector<float, Eigen::Dynamic> norm_collection = target.rowwise().norm();
-    search_scores = search_scores.array() / (norm_query * norm_collection.array());
+    search_scores = search_scores.array() / (norm_query * norm_collection.array() + _eps);
     return search_scores;
 }
 template <typename NumT>
