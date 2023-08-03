ab616b96a1f3f8f174ed084cfbddf1e56fa4dc1b
limengxun <limengxun45@outlook.com>
2023-08-03 22:46:52 +0800

    test

--- tests/test_speed.py
+++ tests/test_speed.py
@@ -1,10 +1,11 @@
 import time
 import numpy as np
 from numpy.testing import assert_approx_equal
+import torch
 from tiny_vectordb import VectorCollection
 
 LEN = 512
-N = 10000
+torch_device = "cpu"
 
 def cos_sim_np(tgt, query):
     """
@@ -16,18 +17,37 @@ def cos_sim_np(tgt, query):
     return np.dot(tgt, query) / (np.linalg.norm(tgt, axis=1) * np.linalg.norm(query))
 
 def search_np(ids: list[str], tgt, query, k):
-    assert isinstance(ids, list)
     assert isinstance(tgt, np.ndarray)
     assert isinstance(query, np.ndarray)
     assert isinstance(k, int)
 
     scores = cos_sim_np(tgt, query)
     topk_indices = np.argsort(scores)[::-1][:k]
-    return [ids[i] for i in topk_indices], scores[topk_indices]
+    # return [ids[i] for i in topk_indices], scores[topk_indices]
+    return ids[topk_indices], scores[topk_indices]
 
-def compareMMTime(n: int):
+def cos_sim_torch(tgt, query):
+    """
+    target: (N, LEN)
+    query: (LEN, )
+    """
+    assert isinstance(tgt, torch.Tensor)
+    assert isinstance(query, torch.Tensor)
+    return torch.matmul(tgt, query) / (torch.norm(tgt, dim=1) * torch.norm(query))
+
+def search_torch(ids: list[str], tgt, query, k):
+    assert isinstance(tgt, torch.Tensor)
+    assert isinstance(query, torch.Tensor)
+    assert isinstance(k, int)
+
+    scores = cos_sim_torch(tgt, query)
+    topk_indices = torch.argsort(scores, descending=True)[:k]
+    # return [ids[i] for i in topk_indices], scores[topk_indices]
+    return ids[topk_indices.cpu().numpy()], scores[topk_indices]
+
+def compare(n: int, k = 10):
     collection = VectorCollection[float](None, "test", LEN, quite_loading=True)
-    print("Compare matrix multiplication for", n, "vectors of length", LEN, ":")
+    print("Compare for", n, "vectors of length", LEN, "numpy | torch", ":")
 
     np.random.seed(0)
     vectors = np.random.rand(n, LEN).tolist()
@@ -40,57 +60,40 @@ def compareMMTime(n: int):
     _t = time.time()
     sc_0 = collection._impl.score(query)
     t_0 = time.time() - _t
-    print("[Eigen] Time taken:", t_0, "seconds")
+    print("[Eigen] Matrix multiplication time taken:", t_0, "seconds")
     _t = time.time()
+    collection.search(query, k)
+    t_01 = time.time() - _t
+    print("[Eigen] Search time taken:", t_01, "seconds")
 
-    target = np.array(target)
-    query = np.array(query)
+    target_np = np.array(target)
+    query_np = np.array(query)
     _t = time.time()
-    sc_1 = cos_sim_np(target, query)
+    sc_1 = cos_sim_np(target_np, query_np)
     t_1 = time.time() - _t
-    print("[Numpy] Time taken:", t_1, "seconds")
+    print("[Numpy] Matrix multiplication time taken:", t_1, "seconds")
+    _t = time.time()
+    search_np(np.array(ids), target_np, query_np, k)
+    t_11 = time.time() - _t
+    print("[Numpy] Search time taken:", t_11, "seconds")
+
+    target_torch = torch.tensor(target).to(torch_device)
+    query_torch = torch.tensor(query).to(torch_device)
+    _t = time.time()
+    sc_2 = cos_sim_torch(target_torch, query_torch)
+    t_2 = time.time() - _t
+    print("[PyTorch] Matrix multiplication time taken:", t_2, "seconds")
+    _t = time.time()
+    search_torch(np.array(ids), target_torch, query_torch, k)
+    t_21 = time.time() - _t
+    print("[PyTorch] Search time taken:", t_21, "seconds")
 
     step = 1 if n < 1000 else n//100
     for i in range(0, n, step):
         assert_approx_equal(sc_0[i], sc_1[i], significant=4)
-    print(f"Matrix multiplication superiority: {t_1 / t_0 : .2f} times faster", end="\n\n")
+    print("----------------------------------")
+    print(f"Matrix mult. superiority: {t_1 / t_0 : .2f} | {t_2 / t_0} times faster", end="\n")
+    print(f"Search superiority: {t_11 / t_01 : .2f} | {t_21 / t_01} times faster", end="\n\n")
 
-def compareSearchTime(n:int, k:int = 16):
-    collection = VectorCollection[float](None, "test", LEN, quite_loading=True)
-    print(f"Compare search time (k={k}) for", n, "vectors of length", LEN, ":")
-
-    np.random.seed(0)
-    vectors = np.random.rand(n, LEN).tolist()
-    ids = [str(x) for x in range(n)]
-
-    query = [float(x) for x in range(LEN)]
-    target = vectors
-
-    collection.addBlock(ids, vectors)
-    _t = time.time()
-    ids_0, scores_0 = collection.search(query, k)
-    t_0 = time.time() - _t
-    print("[Eigen] Time taken:", t_0, "seconds")
-    _t = time.time()
-
-    target = np.array(target)
-    query = np.array(query)
-    _t = time.time()
-    ids_1, scores_1 = search_np(ids, target, query, k)
-    t_1 = time.time() - _t
-    print("[Numpy] Time taken:", t_1, "seconds")
-
-    assert ids_0 == ids_1
-    print(f"Search superiority: {t_1 / t_0 : .2f} times faster", end="\n\n")
-
-
-for i in range(2, 9)[::-1]:
-    compareMMTime(4**i)
-    compareSearchTime(1000, 16)
-
-# compareTime(100000)
-# compareTime(10000)
-# compareTime(5000)
-# compareTime(1000)
-# compareTime(500)
-# compareTime(100)
+for i in range(2,6)[::-1]:
+    compare(10**i)
