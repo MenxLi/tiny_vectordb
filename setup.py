741ec61dc92d7103d791b575248f2fa2a77f3690
limengxun <limengxun45@outlook.com>
2023-12-30 17:01:30 +0800

    0.1.1

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 from setuptools import setup, find_packages
-import os
 
-__this_dir = os.path.dirname(__file__)
+with open("README.md", "r") as f:
+    long_description = f.read()
 
 setup(
     packages=find_packages(),
     include_package_data = True,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
